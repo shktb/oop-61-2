bb5154e11fa0050f046a5b70369b6343ac084906
shktb <dauletsaktybekov@gmail.com>
2025-12-19 17:56:03 +0600

    added lesson 6

---
+++ lessons/lesson_6.py
@@ -0,0 +1,15 @@
+import random
+from random import randint, randrange
+import datetime
+import time
+import os
+import pathlib
+
+# print(randint(1, 10))
+
+# # import lessonn1
+# from lessonn1 import Hero
+#
+# hero = Hero('Aradger', 11, 100)
+#
+# print(hero.name)
